42f740b8729830901db7daa6cd0a4b94243d2405
lmcez <lmcez@LUCAS>
2024-07-09 08:52:01 -0300

    Add project files.

---
+++ src/Journey.Application/UseCases/GetAll/GetAllTripsUseCase.cs
@@ -0,0 +1,26 @@
+﻿using Journey.Communication.Responses;
+using Journey.Infrastructure;
+
+namespace Journey.Application.UseCases.GetAll
+{
+    public class GetAllTripsUseCase
+    {
+        public ResponseTripsJson Execute()
+        {
+            var dbContext = new JourneyDbContext();
+
+            var trips = dbContext.Trips.ToList();
+
+            return new ResponseTripsJson
+            {
+                Trips = trips.Select(trip => new ResponseShortTripJson
+                {
+                    Id = trip.Id,
+                    StartDate = trip.StartDate,
+                    EndDate = trip.EndDate,
+                    Name = trip.Name,
+                }).ToList()
+            };
+        }
+    }
+}
