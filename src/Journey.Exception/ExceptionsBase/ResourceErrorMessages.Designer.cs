52ea5f5bd81f8edbd792120b10b94de94cba3e18
Lucas Cezar <lmcezar@gmail.com>
2024-07-14 11:12:43 -0300

    feat: Aula 3 - Implementação do método DELETE e UPDATE para Trips e Activities

--- src/Journey.Exception/ExceptionsBase/ResourceErrorMessages.Designer.cs
+++ src/Journey.Exception/ExceptionsBase/ResourceErrorMessages.Designer.cs
@@ -61,6 +61,24 @@ namespace Journey.Exception.ExceptionsBase {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Activity not found..
+        /// </summary>
+        public static string ACTIVITY_NOT_FOUND {
+            get {
+                return ResourceManager.GetString("ACTIVITY_NOT_FOUND", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The date you selected for the activity is not within your travel period..
+        /// </summary>
+        public static string DATE_NOT_WHITIN_TRAVEL_PERIOD {
+            get {
+                return ResourceManager.GetString("DATE_NOT_WHITIN_TRAVEL_PERIOD", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The date of the trip must be later than today..
         /// </summary>
         public static string DATE_TRIP_MUST_BR_LATER_THAN_TODAY {
@@ -93,6 +111,15 @@ namespace Journey.Exception.ExceptionsBase {
         public static string TRIP_NOT_FOUND {
             get {
                 return ResourceManager.GetString("TRIP_NOT_FOUND", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Unknown error..
+        /// </summary>
+        public static string UNKNOWN_ERROR {
+            get {
+                return ResourceManager.GetString("UNKNOWN_ERROR", resourceCulture);
             }
         }
     }
